a7beeae32bc8cb986fd80cb0c3ec1191660e9a69
Evabot <yelegilbertevariste@gmail.com>
2025-04-17 10:46:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
-'use strict';
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 
-const Constants = require('./src/util/Constants');
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    headless: true,
+    args: ['--no-sandbox']
+  }
+});
 
-module.exports = {
-    Client: require('./src/Client'),
-    
-    version: require('./package.json').version,
+client.on('qr', qr => {
+  qrcode.generate(qr, { small: true });
+});
 
-    // Structures
-    Chat: require('./src/structures/Chat'),
-    PrivateChat: require('./src/structures/PrivateChat'),
-    GroupChat: require('./src/structures/GroupChat'),
-    Message: require('./src/structures/Message'),
-    MessageMedia: require('./src/structures/MessageMedia'),
-    Contact: require('./src/structures/Contact'),
-    PrivateContact: require('./src/structures/PrivateContact'),
-    BusinessContact: require('./src/structures/BusinessContact'),
-    ClientInfo: require('./src/structures/ClientInfo'),
-    Location: require('./src/structures/Location'),
-    Poll: require('./src/structures/Poll'),
-    ProductMetadata: require('./src/structures/ProductMetadata'),
-    List: require('./src/structures/List'),
-    Buttons: require('./src/structures/Buttons'),
-    Broadcast: require('./src/structures/Broadcast'),
-    
-    // Auth Strategies
-    NoAuth: require('./src/authStrategies/NoAuth'),
-    LocalAuth: require('./src/authStrategies/LocalAuth'),
-    RemoteAuth: require('./src/authStrategies/RemoteAuth'),
-    
-    ...Constants
-};
+client.on('ready', () => {
+  console.log('Bot WhatsApp prêt !');
+});
+
+client.on('message', async message => {
+  if (message.body === '!ping') {
+    await message.reply('pong');
+  }
+});
+
+client.initialize();
